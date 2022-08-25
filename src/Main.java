9abd7f04fbcc02c50ddbf7d4e626892b1bb7f79b
jah <celt8@yandex.ru>
2022-08-25 12:15:12 +0500

    wer

--- src/Main.java
+++ src/Main.java
@@ -27,5 +27,29 @@ public class Main {
             System.out.println(g);
         }
 
+        //Dop Exercise 1
+        for(int h =1; h<=30; h++){
+            if(h % 3 == 0 && h % 5 == 0 ){
+                System.out.println(h+" ping pong");
+            }else if(h % 3 == 0){
+                System.out.println(h+" ping");
+            }else if(h % 5 == 0){
+                System.out.println(h+" pong");
+            }else{
+                System.out.println(h);
+            }
+        }
+
+        //Dop Exercise 2
+        int f = 0;
+        int s = 1;
+        int fib =0;
+        System.out.print(f+" "+s+" ");
+        for(int u = 2; u <10; u++){
+            fib = f+s;
+            System.out.print(fib+" ");
+            f = s;
+            s= fib;
+        }
     }
 }
